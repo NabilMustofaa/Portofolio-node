674e7b4fdb2e373c8f1bce91007d5d539a66c4c3
NabilMustofaa <nabilmustofa6@gmail.com>
2022-10-26 14:25:20 +0700

    Node JS detail finish

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+
+
+require('./utils/db');
+const portos = require('./models/portos');
 
 const app = express();
 const port = 3000;
@@ -10,23 +17,38 @@ app.use(expressLayouts);
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 
+
+//session setup
+app.use(cookieParser('secret'));
+app.use(session({
+    cookie: { maxAge: 60000 },
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+
 //routes
 app.get('/', (req, res) => {
     res.render('index',
         {
-            title: 'Home',
-            layout: 'layouts/main'
+            title: 'home',
+            layout: 'layouts/main',
+            id: 'home'
             
         }
     );  
 });
 //detail
-app.get('/detail', (req, res) => {
+app.get('/detail/:name', async (req, res) => {
+    const porto = await portos.findOne({name: req.params.name});
+    
     res.render('detail',
         {
             title: 'Detail',
-            layout: 'layouts/child'
-            
+            layout: 'layouts/main',
+            id: 'detail',
+            porto
         }
     );  
 });
--- utils/db.js
+++ utils/db.js
@@ -6,31 +6,31 @@ mongoose.connect("mongodb+srv://nabilmustofaa:nabil123@portofolio.ou0dis1.mongod
     useCreateIndex: true
 });
 
-//membuat schema
-const portos = mongoose.model('portos', {
-    name: String,
-    description: String,
-    githubLink: String,
-    liveLink: String,
-    imageArray: Array,
-    technologyArray: Array,
-});
+// //membuat schema
+// const portos = mongoose.model('portos', {
+//     name: String,
+//     description: String,
+//     githubLink: String,
+//     liveLink: String,
+//     imageArray: Array,
+//     technologyArray: Array,
+// });
 
 // menambah 1 data
-const item= new portos({
-    name: "Portofolio Website",
-    description: "Portofolio Website ini dibuat dalam rangka kebutuhan branding diri saya sebagai seorang web developer. Website portofolio ini sendiri telah dikembangkan beberapa kali sejak pertama kali dibuat. Website portofolio ini pada awalnya dibuat dengan menggunakan HTML, CSS, dan Javascript. Namun seiring berjalannya waktu mulai mengenal beberapa framework dalam pengembangan website seperti node.js, express.js, dan laravel. Untuk pengembangan website portofolio ini sendiri saya menggunakan gabungan framework node.js dan express.js. Website portofolio ini sendiri dibuat dengan menggunakan template engine dari ejs. Penggunaan database yang digunakan dalam website portofolio ini cukup minimal, hanya digunakan untuk menyimpan data-data portofolio yang saya buat. Untuk database yang digunakan dalam website portofolio ini sendiri saya mencoba menggunakan platfrom no sql yaitu mongodb. Rencana pengembangan website portofolio ini sendiri adalah untuk menggunakan framework javascript lainnya seperti react.js dan vue.js. Sehingga nantinya website ini akan secara penuh dikemabangkan menggunakan MERN atau MEVN stack.",
-    githubLink: "https://github",
-    liveLink: "https://github",
-    imageArray: [
-        "1.png","2.png","3.png"
-    ],
-    technologyArray: [
-        "Node.png","Express.png","MongoDB.png","EJS.png","Bootstrap.png"
-    ],
+// const item= new portos({
+//     name: "Portofolio Website",
+//     description: "Portofolio Website ini dibuat dalam rangka kebutuhan branding diri saya sebagai seorang web developer. Website portofolio ini sendiri telah dikembangkan beberapa kali sejak pertama kali dibuat. Website portofolio ini pada awalnya dibuat dengan menggunakan HTML, CSS, dan Javascript. Namun seiring berjalannya waktu mulai mengenal beberapa framework dalam pengembangan website seperti node.js, express.js, dan laravel. Untuk pengembangan website portofolio ini sendiri saya menggunakan gabungan framework node.js dan express.js. Website portofolio ini sendiri dibuat dengan menggunakan template engine dari ejs. Penggunaan database yang digunakan dalam website portofolio ini cukup minimal, hanya digunakan untuk menyimpan data-data portofolio yang saya buat. Untuk database yang digunakan dalam website portofolio ini sendiri saya mencoba menggunakan platfrom no sql yaitu mongodb. Rencana pengembangan website portofolio ini sendiri adalah untuk menggunakan framework javascript lainnya seperti react.js dan vue.js. Sehingga nantinya website ini akan secara penuh dikemabangkan menggunakan MERN atau MEVN stack.",
+//     githubLink: "https://github.com/NabilMustofaa/Portofolio-node",
+//     liveLink: "https://nabilmustofa.my.id/",
+//     imageArray: [
+//         "1.png","2.png","3.png"
+//     ],
+//     technologyArray: [
+//         "Node.png","Express.png","MongoDB.png","EJS.png","Bootstrap.png"
+//     ],
 
-});
+// });
 
-item.save().then((result) => {
-    console.log(result);
-});
+// item.save().then((result) => {
+//     console.log(result);
+// });
