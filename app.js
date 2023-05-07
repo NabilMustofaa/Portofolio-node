9a54aa959e98b31f2898e4d7d2f420cdd2b4655f
NabilMustofaa <nabilmustofa6@gmail.com>
2023-05-07 16:08:24 +0700

    update gcloud buckets

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const flash = require('connect-flash');
 
-
-require('./utils/db');
-const portos = require('./models/portos');
 
 const app = express();
 const port = 3000;
@@ -16,16 +10,6 @@ app.set('view engine','ejs')
 app.use(expressLayouts);
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
-
-
-//session setup
-app.use(cookieParser('secret'));
-app.use(session({
-    cookie: { maxAge: 60000 },
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
 
 
 //routes
@@ -39,20 +23,6 @@ app.get('/', (req, res) => {
         }
     );  
 });
-//detail
-app.get('/detail/:name', async (req, res) => {
-    const porto = await portos.findOne({name: req.params.name});
-    
-    res.render('detail',
-        {
-            title: 'Detail',
-            layout: 'layouts/main',
-            id: 'detail',
-            porto
-        }
-    );  
-});
-
 
 
 app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
